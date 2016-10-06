fb9d3feb69b52860e8a0ab7b4afb302b6ce213d5
Chris Kimberley <ckimbe02@dcs.bbk.ac.uk>
2016-10-06 18:06:55 +0100

    Initial commit

---
+++ src/ComparatorTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created by ckimbe02 on 06/10/2016.
+ */
+public class ComparatorTest {
+    public static void main(String[] args) {
+        new ComparatorTest().launch();
+    }
+
+    private void launch() {
+        
+    }
+}
